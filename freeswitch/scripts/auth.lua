d22371acdbba66047dfe505a8414c0371b8672e1
Attila Gulyas <toraritte@gmail.com>
2019-06-26 18:17:51 +0000

    FS: re-structure lua script

--- freeswitch/scripts/auth.lua
+++ freeswitch/scripts/auth.lua
@@ -1,5 +1,5 @@
-
 ani = session:getVariable("ani")
+freeswitch.consoleLog("INFO", "lua script: ani = " .. ani)
 
 conn_string =
   "pgsql://hostaddr=10.142.0.2"              ..
@@ -14,45 +14,47 @@ dbh = freeswitch.Dbh(conn_string)
 assert(dbh:connected())
 freeswitch.consoleLog("INFO", "lua script: connected to DB")
 
--- NOTE: Workaround for when query returns no rows.
---       See https://stackoverflow.com/questions/56744392
-q =
-  "SELECT COALESCE("                                  ..
-  "  (SELECT '+1' || phone_number"                    .. -- \
-  "    FROM phone_numbers"                            .. --  > subquery
-  "    WHERE '+1' || phone_number = '" .. ani .. "')" .. -- /
-  "  , 'unauthorized'"                                .. -- return "unauthorized" if no matching rows
-  ")"
-
 session:execute("sleep", "750")
 session:execute("speak", "flite|slt|Welcome to Access News!")
 
-dbh:query(q, function(row)
+function ani_registered(ani)
 
-  freeswitch.consoleLog("INFO", "lua script: ani = " .. ani)
+  local registered = false
 
-  if row.coalesce == "unauthorized" then
-    freeswitch.consoleLog("INFO", "lua script: " .. ani .. " not in database, hanging up")
+  local q =
+    "SELECT '+1' || phone_number"                    ..
+    "  FROM phone_numbers"                           ..
+    "  WHERE '+1' || phone_number = '" .. ani .. "'"
 
-    -- TODO: ask for the PIN at this point (last 4 digits
-    -- of registered number) and if that fails, then kick
-    -- into demo mode
+  dbh:query(q, function(row)
+    registered = true
+  end)
 
-    -- flite is not aware of punctuation marks therefore
-    -- the entire text is read in one long string, but it
-    -- is just a placeholder right now anyway.
-    local demo_note =
-      "Please note that you are currently in demo"           ..
-      "mode and the system will hang up in 5 minutes."       ..
-      "You can register any time by calling 916, 889, 7519."
-    session:execute("speak", "flite|slt|" .. demo_note)
-    session:execute("sched_hangup", "+300 allotted_timeout")
-    -- session:hangup()
-  end
+  return registered
+end
 
-  session:execute("sleep", "750")
-  session:execute("ivr","demo_ivr")
+if ani_registered(ani) == false then
+  freeswitch.consoleLog("INFO", ani .. " is not registered")
 
-end)
+  -- TODO: ask for the PIN at this point (last 4 digits
+  -- of registered number) and if that fails, then kick
+  -- into demo mode
+
+  -- flite is not aware of punctuation marks therefore
+  -- the entire text is read in one long string, but it
+  -- is just a placeholder right now anyway.
+  local demo_note =
+    "Please note that you are currently in demo"           ..
+    "mode and the system will hang up in 5 minutes."       ..
+    "You can register any time by calling 916, 889, 7519."
+
+  session:execute("speak", "flite|slt|" .. demo_note)
+  session:execute("sched_hangup", "+300 allotted_timeout")
+end
+
+freeswitch.consoleLog("INFO", ani .. " registered")
+
+session:execute("sleep", "750")
+session:execute("ivr","demo_ivr")
 
 dbh:release()
