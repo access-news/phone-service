968dd241894eaf15f50055854a3fbec0c8a4cb04
Attila Gulyas <toraritte@gmail.com>
2019-06-26 00:01:21 +0000

    freeswitch: add sched_hangup to calls from unregistered numbers

--- freeswitch/scripts/auth.lua
+++ freeswitch/scripts/auth.lua
@@ -17,20 +17,33 @@ freeswitch.consoleLog("INFO", "lua script: connected to DB")
 -- NOTE: Workaround for when query returns no rows.
 --       See https://stackoverflow.com/questions/56744392
 q =
-  "SELECT COALESCE(" ..
-    "(SELECT '+1' || phone_number " ..
-      "FROM phone_numbers " ..
-      "WHERE '+1' || phone_number = '" .. ani .. "')" .. 
-    ", '0'" ..
+  "SELECT COALESCE("                                  ..
+  "  (SELECT '+1' || phone_number"                    .. -- \
+  "    FROM phone_numbers"                            .. --  > subquery
+  "    WHERE '+1' || phone_number = '" .. ani .. "')" .. -- /
+  "  , 'unauthorized'"                                .. -- return "unauthorized" if no matching rows
   ")"
+
+session:execute("sleep", "750")
+session:execute("speak", "flite|slt|Welcome to Access News!")
 
 dbh:query(q, function(row)
 
   freeswitch.consoleLog("INFO", "lua script: ani = " .. ani)
 
-  if row.coalesce == "0" then
+  if row.coalesce == "unauthorized" then
     freeswitch.consoleLog("INFO", "lua script: " .. ani .. " not in database, hanging up")
-    session:hangup()
+
+    -- flite is not aware of punctuation marks therefore
+    -- the entire text is read in one long string, but it
+    -- is just a placeholder right now anyway.
+    local demo_note =
+      "Please note that you are currently in demo"           ..
+      "mode and the system will hang up in 5 minutes."       ..
+      "You can register any time by calling 916, 889, 7519."
+    session:execute("speak", "flite|slt|" .. demo_note)
+    session:execute("sched_hangup", "+300 allotted_timeout")
+    -- session:hangup()
   end
 
   session:execute("sleep", "750")
