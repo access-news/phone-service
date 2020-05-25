5c93189f8a6de7a03319dee4f141af9faa89932f
Attila Gulyas <toraritte@gmail.com>
2020-05-25 07:18:06 -0700

    [google-tts-wav.sh] Add gcloud dep and `export`

--- google-tts-wav.sh
+++ google-tts-wav.sh
@@ -9,10 +9,13 @@
 
 # Note: All links below have been saved to web.archive.org on 2020/05/23.
 
-# DEPENDENCIES: curl, jq, base64
+# DEPENDENCIES: gcloud, curl, jq, base64
 
-# Easiest way to install them:
+# Easiest way to install them is using Nix:
+# # straightforward:
 # $ nix-env -iA nixpkgs.jq
+# $ nix-env -iA nixpkgs.google-cloud-sdk
+# # if nixos/nixpkgs is cloned:
 # $ nix-env -f path/to/cloned/nixpkgs -iA pkgs.curl
 
 # OTHER REQUIRED STEPS BEFORE USING
@@ -31,8 +34,20 @@
 #    $ scp google-tts-wav.sh blabla-92618-bec1d0a875c7.json  52.1.2.3:/to/dir
 
 # 3. create environment variable with credentials file
+#    (i.e., edit the line below)
 #
 #    $ export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/blabla-92618-bec1d0a875c7.json"
+
+# NOTE Hardcoding it here until steps to production are finalized {{-
+# Rationale: Was running this in a dev shell from the app, but it
+#            was not  working correctly  as the  `gcloud` command
+#            below did not find the  credentials - because it has
+#            not  been exported  when the  initial ERL  shell was
+#            started. This way the  script is self-contained, but
+#            requires editing  whenever the service  account JSON
+#            file is replaced.
+# }}-
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/your-service-account.json"
 
 # For the available REST request options, see
 # https://cloud.google.com/text-to-speech/docs/reference/rest/v1beta1/text/synthesize
@@ -64,3 +79,6 @@ curl -X POST \
 jq --raw-output ".audioContent" \
 | \
 base64 --decode
+
+# vim: set fdm=marker:
+# vim: set foldmarker={{-,}}-:
